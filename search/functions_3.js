21d03f4fefd901f58481f774631cf03b8b35681e
gvegayon <gvegayon@users.noreply.github.com>
2023-04-25 17:24:27 +0000

    deploy: 5b77e74abad02ef081ed62c187d81ed94c77ae4a

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['generation_5ftime_132',['generation_time',['../classepiworld_1_1_data_base.html#a17a16f7aa666f4852b40324cf89cd449',1,'epiworld::DataBase::generation_time()'],['../class_data_base.html#a17a16f7aa666f4852b40324cf89cd449',1,'DataBase::generation_time()']]],
-  ['get_5fentities_133',['get_entities',['../classepiworld_1_1_agent.html#aa8f3cf3ff2cd28ab97b6489b2d5124a8',1,'epiworld::Agent::get_entities()'],['../class_agent.html#aa8f3cf3ff2cd28ab97b6489b2d5124a8',1,'Agent::get_entities()']]],
-  ['get_5fid_134',['get_id',['../classepiworld_1_1_agent.html#a3763f12b56e8dd7f0e23bbca4f183c8a',1,'epiworld::Agent::get_id()'],['../class_agent.html#a3763f12b56e8dd7f0e23bbca4f183c8a',1,'Agent::get_id()']]],
-  ['get_5fqueue_135',['get_queue',['../classepiworld_1_1_model.html#a3cf5b4f14dcd54816785b38894c8088f',1,'epiworld::Model::get_queue()'],['../class_model.html#a3cf5b4f14dcd54816785b38894c8088f',1,'Model::get_queue()']]]
+  ['get_5fid_133',['get_id',['../classepiworld_1_1_agent.html#a3763f12b56e8dd7f0e23bbca4f183c8a',1,'epiworld::Agent::get_id()'],['../class_agent.html#a3763f12b56e8dd7f0e23bbca4f183c8a',1,'Agent::get_id()']]],
+  ['get_5fqueue_134',['get_queue',['../classepiworld_1_1_model.html#a3cf5b4f14dcd54816785b38894c8088f',1,'epiworld::Model::get_queue()'],['../class_model.html#a3cf5b4f14dcd54816785b38894c8088f',1,'Model::get_queue()']]]
 ];
