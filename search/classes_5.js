c033b3bb1391d72b173813456e1f5b9a583b83e2
gvegayon <gvegayon@users.noreply.github.com>
2023-06-06 23:14:06 +0000

    deploy: f2b5d8c0689fc7b0e96f7f51cfd275d90579a0ab

--- search/classes_5.js
+++ search/classes_5.js
@@ -3,7 +3,7 @@ var searchData=
   ['model_98',['Model',['../classepiworld_1_1_model.html',1,'epiworld::Model&lt; TSeq &gt;'],['../class_model.html',1,'Model&lt; TSeq &gt;']]],
   ['model_3c_20epi_5fdefault_5ftseq_20_3e_99',['Model&lt; EPI_DEFAULT_TSEQ &gt;',['../classepiworld_1_1_model.html',1,'epiworld']]],
   ['model_3c_20int_20_3e_100',['Model&lt; int &gt;',['../classepiworld_1_1_model.html',1,'epiworld']]],
-  ['modeldiffnet_101',['ModelDiffNet',['../class_model_diff_net.html',1,'']]],
+  ['modeldiffnet_101',['ModelDiffNet',['../classepiworld_1_1epimodels_1_1_model_diff_net.html',1,'epiworld::epimodels::ModelDiffNet&lt; TSeq &gt;'],['../class_model_diff_net.html',1,'ModelDiffNet&lt; TSeq &gt;']]],
   ['modelseir_102',['ModelSEIR',['../classepiworld_1_1epimodels_1_1_model_s_e_i_r.html',1,'epiworld::epimodels::ModelSEIR&lt; TSeq &gt;'],['../class_model_s_e_i_r.html',1,'ModelSEIR&lt; TSeq &gt;']]],
   ['modelseirconn_103',['ModelSEIRCONN',['../classepiworld_1_1epimodels_1_1_model_s_e_i_r_c_o_n_n.html',1,'epiworld::epimodels::ModelSEIRCONN&lt; TSeq &gt;'],['../class_model_s_e_i_r_c_o_n_n.html',1,'ModelSEIRCONN&lt; TSeq &gt;']]],
   ['modelseirconnlogit_104',['ModelSEIRCONNLogit',['../class_model_s_e_i_r_c_o_n_n_logit.html',1,'']]],
