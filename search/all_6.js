ec3c08733860bbff79387e39fae698f4029adf25
gvegayon <gvegayon@users.noreply.github.com>
2023-10-12 19:58:32 +0000

    deploy: ed87d74617b0b0788b4bad5be7ff6d09406540f9

--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['initial_5fstates_33',['initial_states',['../class_model_s_e_i_r.html#a749d2d7b995dd385712e7ea8a35ebe28',1,'ModelSEIR::initial_states()'],['../class_model_s_i_r.html#add6b7af95b87bef7f661f0ec90945821',1,'ModelSIR::initial_states()']]],
+  ['initial_5fstates_33',['initial_states',['../class_model_s_e_i_r.html#a749d2d7b995dd385712e7ea8a35ebe28',1,'ModelSEIR::initial_states()'],['../class_model_s_e_i_r_c_o_n_n.html#a444e6f5b9ebdcd108feaea16e98c80df',1,'ModelSEIRCONN::initial_states()'],['../class_model_s_i_r.html#add6b7af95b87bef7f661f0ec90945821',1,'ModelSIR::initial_states()'],['../class_model_s_i_r_c_o_n_n.html#abaa17ce4d249250f4fbe64d54c3863a8',1,'ModelSIRCONN::initial_states()'],['../class_model_s_i_r_d.html#a1db04f61078c9f5a52fca4761a589cf4',1,'ModelSIRD::initial_states()']]],
   ['initial_5fstates_5ffun_34',['initial_states_fun',['../class_model.html#a6a3e893c076226874afbf0c0b20804ac',1,'Model']]],
   ['is_5fdirected_35',['is_directed',['../classepiworld_1_1_adj_list.html#a70e14a9f262278d028cecae547382049',1,'epiworld::AdjList::is_directed()'],['../class_adj_list.html#a70e14a9f262278d028cecae547382049',1,'AdjList::is_directed()']]],
   ['is_5fqueuing_5fon_36',['is_queuing_on',['../classepiworld_1_1_model.html#a2c4ac58e918e9e8dc2e58be6521d8827',1,'epiworld::Model::is_queuing_on()'],['../class_model.html#a2c4ac58e918e9e8dc2e58be6521d8827',1,'Model::is_queuing_on()']]]
