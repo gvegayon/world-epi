1b1cc7a86b7fda2cf3522a2538eea3a56cb1b0df
gvegayon <gvegayon@users.noreply.github.com>
2024-04-15 06:08:38 +0000

    deploy: ed0f80da3b597b10361e6c44acdc632a6d188b71

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['lfmcmc_39',['LFMCMC',['../classepiworld_1_1_l_f_m_c_m_c.html',1,'epiworld::LFMCMC&lt; TData &gt;'],['../class_l_f_m_c_m_c.html',1,'LFMCMC&lt; TData &gt;']]],
-  ['load_5fagents_5fentities_5fties_40',['load_agents_entities_ties',['../classepiworld_1_1_model.html#a4761b43494d081f2f7bd2443ea6cb2db',1,'epiworld::Model::load_agents_entities_ties()'],['../class_model.html#a4761b43494d081f2f7bd2443ea6cb2db',1,'Model::load_agents_entities_ties()']]]
+  ['lfmcmc_38',['LFMCMC',['../classepiworld_1_1_l_f_m_c_m_c.html',1,'epiworld::LFMCMC&lt; TData &gt;'],['../class_l_f_m_c_m_c.html',1,'LFMCMC&lt; TData &gt;']]],
+  ['load_5fagents_5fentities_5fties_39',['load_agents_entities_ties',['../classepiworld_1_1_model.html#a4761b43494d081f2f7bd2443ea6cb2db',1,'epiworld::Model::load_agents_entities_ties()'],['../class_model.html#a4761b43494d081f2f7bd2443ea6cb2db',1,'Model::load_agents_entities_ties()']]]
 ];
--- search/classes_3.js
+++ search/classes_3.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['globalevent_111',['GlobalEvent',['../classepiworld_1_1_global_event.html',1,'epiworld::GlobalEvent&lt; TSeq &gt;'],['../class_global_event.html',1,'GlobalEvent&lt; TSeq &gt;']]],
-  ['groupsampler_112',['GroupSampler',['../class_group_sampler.html',1,'']]],
-  ['groupsampler_3c_20epi_5fdefault_5ftseq_20_3e_113',['GroupSampler&lt; EPI_DEFAULT_TSEQ &gt;',['../class_group_sampler.html',1,'']]]
+  ['globalevent_110',['GlobalEvent',['../classepiworld_1_1_global_event.html',1,'epiworld::GlobalEvent&lt; TSeq &gt;'],['../class_global_event.html',1,'GlobalEvent&lt; TSeq &gt;']]],
+  ['groupsampler_111',['GroupSampler',['../class_group_sampler.html',1,'']]]
 ];
