9da576770af51cb4b42cc7cbe6ef2842326beead
gvegayon <gvegayon@users.noreply.github.com>
2024-04-27 22:22:42 +0000

    deploy: 4108378fe6c5b2ba13a3af2f48b57ec190c42f56

--- search/classes_5.js
+++ search/classes_5.js
@@ -8,7 +8,7 @@ var searchData=
   ['modelseirconn_119',['ModelSEIRCONN',['../classepiworld_1_1epimodels_1_1_model_s_e_i_r_c_o_n_n.html',1,'epiworld::epimodels::ModelSEIRCONN&lt; TSeq &gt;'],['../class_model_s_e_i_r_c_o_n_n.html',1,'ModelSEIRCONN&lt; TSeq &gt;']]],
   ['modelseird_120',['ModelSEIRD',['../classepiworld_1_1epimodels_1_1_model_s_e_i_r_d.html',1,'epiworld::epimodels::ModelSEIRD&lt; TSeq &gt;'],['../class_model_s_e_i_r_d.html',1,'ModelSEIRD&lt; TSeq &gt;']]],
   ['modelseirdconn_121',['ModelSEIRDCONN',['../classepiworld_1_1epimodels_1_1_model_s_e_i_r_d_c_o_n_n.html',1,'epiworld::epimodels::ModelSEIRDCONN&lt; TSeq &gt;'],['../class_model_s_e_i_r_d_c_o_n_n.html',1,'ModelSEIRDCONN&lt; TSeq &gt;']]],
-  ['modelseirmixing_122',['ModelSEIRMixing',['../class_model_s_e_i_r_mixing.html',1,'']]],
+  ['modelseirmixing_122',['ModelSEIRMixing',['../classepiworld_1_1epimodels_1_1_model_s_e_i_r_mixing.html',1,'epiworld::epimodels::ModelSEIRMixing&lt; TSeq &gt;'],['../class_model_s_e_i_r_mixing.html',1,'ModelSEIRMixing&lt; TSeq &gt;']]],
   ['modelsir_123',['ModelSIR',['../classepiworld_1_1epimodels_1_1_model_s_i_r.html',1,'epiworld::epimodels::ModelSIR&lt; TSeq &gt;'],['../class_model_s_i_r.html',1,'ModelSIR&lt; TSeq &gt;']]],
   ['modelsirconn_124',['ModelSIRCONN',['../classepiworld_1_1epimodels_1_1_model_s_i_r_c_o_n_n.html',1,'epiworld::epimodels::ModelSIRCONN&lt; TSeq &gt;'],['../class_model_s_i_r_c_o_n_n.html',1,'ModelSIRCONN&lt; TSeq &gt;']]],
   ['modelsird_125',['ModelSIRD',['../classepiworld_1_1epimodels_1_1_model_s_i_r_d.html',1,'epiworld::epimodels::ModelSIRD&lt; TSeq &gt;'],['../class_model_s_i_r_d.html',1,'ModelSIRD&lt; TSeq &gt;']]],
