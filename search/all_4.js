a3cbfc62ddb452ce718998fbb6ea7934e9ccd313
gvegayon <gvegayon@users.noreply.github.com>
2024-06-06 21:31:10 +0000

    deploy: cf1c574dbcd1f6dfa334fb7bc692da991fcbb381

--- search/all_4.js
+++ search/all_4.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['ecount_10',['ecount',['../classepiworld_1_1_adj_list.html#a776cefb7cc85d544e4eb9e61aaa46f83',1,'epiworld::AdjList::ecount()'],['../class_adj_list.html#a776cefb7cc85d544e4eb9e61aaa46f83',1,'AdjList::ecount()']]],
+  ['ecount_10',['ecount',['../class_adj_list.html#a776cefb7cc85d544e4eb9e61aaa46f83',1,'AdjList::ecount()'],['../classepiworld_1_1_adj_list.html#a776cefb7cc85d544e4eb9e61aaa46f83',1,'epiworld::AdjList::ecount()']]],
   ['entities_11',['Entities',['../class_entities.html',1,'Entities&lt; TSeq &gt;'],['../classepiworld_1_1_entities.html',1,'epiworld::Entities&lt; TSeq &gt;']]],
   ['entities_5fconst_12',['Entities_const',['../class_entities__const.html',1,'Entities_const&lt; TSeq &gt;'],['../classepiworld_1_1_entities__const.html',1,'epiworld::Entities_const&lt; TSeq &gt;']]],
-  ['entity_13',['Entity',['../class_entity.html',1,'Entity&lt; TSeq &gt;'],['../classepiworld_1_1_entity.html',1,'epiworld::Entity&lt; TSeq &gt;']]],
+  ['entity_13',['Entity',['../class_entity.html#a6e9bb0ff316ea7367e29a53b511a15f6',1,'Entity::Entity()'],['../classepiworld_1_1_entity.html#ae7b1bdf38fe261809f4ba03b5ee1bd69',1,'epiworld::Entity::Entity()'],['../class_entity.html',1,'Entity&lt; TSeq &gt;'],['../classepiworld_1_1_entity.html',1,'epiworld::Entity&lt; TSeq &gt;']]],
   ['epi_20simulator_14',['EPI Simulator',['../md__r_e_a_d_m_e_old.html',1,'']]],
   ['epiworld_20c_2b_2b_20template_20library_15',['epiworld c++ template library',['../md_examples_07_surveillance_07_surveillance.html',1,'']]],
-  ['event_16',['Event',['../structepiworld_1_1_event.html',1,'epiworld::Event&lt; TSeq &gt;'],['../structepiworld_1_1_event.html#ab711f7fd47bbffdfe1e4325e9b327004',1,'epiworld::Event::Event()'],['../struct_event.html#a3b0dd668c6413ed1e622e20acd0a5cdf',1,'Event::Event()'],['../struct_event.html',1,'Event&lt; TSeq &gt;']]],
+  ['event_16',['Event',['../structepiworld_1_1_event.html',1,'epiworld::Event&lt; TSeq &gt;'],['../struct_event.html',1,'Event&lt; TSeq &gt;'],['../structepiworld_1_1_event.html#ab711f7fd47bbffdfe1e4325e9b327004',1,'epiworld::Event::Event()'],['../struct_event.html#a3b0dd668c6413ed1e622e20acd0a5cdf',1,'Event::Event()']]],
   ['events_17',['events',['../classepiworld_1_1_model.html#a8ea9aacbf89d9f757e6b01f74335fb07',1,'epiworld::Model::events()'],['../class_model.html#a13d663322629f76f7e9462b6572475d8',1,'Model::events()']]],
   ['events_5fadd_18',['events_add',['../classepiworld_1_1_model.html#a5ed705d4e368beb431fd8d1cf1d1f2d2',1,'epiworld::Model::events_add()'],['../class_model.html#a5ed705d4e368beb431fd8d1cf1d1f2d2',1,'Model::events_add()']]],
   ['events_5frun_19',['events_run',['../classepiworld_1_1_model.html#a800647b05836b20054f1f2049a41787f',1,'epiworld::Model::events_run()'],['../class_model.html#a800647b05836b20054f1f2049a41787f',1,'Model::events_run()']]],
